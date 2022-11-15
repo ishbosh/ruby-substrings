5c2cd1974f5dba887ce6c716332dac0dcbbb2f63
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-11-15 12:06:16 -0700

    Add error checking

--- script.rb
+++ script.rb
@@ -1,8 +1,20 @@
 def substrings(str, substr_array)
+    unless str.class == String
+        puts "First argument must be a string." 
+        return
+    end 
+    unless substr_array.class == Array
+        puts "Second argument must be an array." 
+        return 
+    end
     result = {}
     str = str.downcase
     # check if the str includes each substr
     substr_array.each do |substr|
+        unless substr.class == String
+            puts "Array must contain only strings."
+            return result = nil
+        end
         substr = substr.downcase
         if str.include?(substr)
             result[substr] = str.split(substr).length - 1
