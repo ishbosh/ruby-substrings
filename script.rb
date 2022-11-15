d7f00c4e709889b551abb9e2efaa9e5b967ed18f
ishbosh <99460383+ishbosh@users.noreply.github.com>
2022-11-15 11:56:06 -0700

    Add case insensitivity

--- script.rb
+++ script.rb
@@ -1,10 +1,12 @@
 def substrings(str, substr_array)
     result = {}
+    str = str.downcase
     # check if the str includes each substr
     substr_array.each do |substr|
+        substr = substr.downcase
         if str.include?(substr)
-            result[substr.to_sym] = str.split(substr).length - 1
+            result[substr] = str.split(substr).length - 1
         end
     end
     result
-end
+end 
